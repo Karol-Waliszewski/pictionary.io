e2dee0282c1c52e378abf335d647476a1e097fd7
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-01-13 23:36:30 +0100

    Created Room joining,leaving and creation

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="createRoom">Create Room</button>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+  methods: {
+    createRoom() {
+      this.$socket.emit("create_room", {
+        name: "Testowy pokoj",
+        isPrivate: true,
+        password: "test",
+        maxUsers: 5
+      });
+    }
+  }
+};
+</script>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+const createID = require("uniqid");
+
+var ROOMS = {};
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
@@ -20,6 +24,78 @@ io.on("connection", socket => {
   socket.on("hello", msg => {
     console.log(`Hello: ${socket.id}, ${msg}`);
     io.emit("hello", msg);
+
+    console.log(ROOMS);
+  });
+
+  // Creating the room
+  socket.on("create_room", options => {
+    if (typeof options.name != "undefined" || options.name.length <= 0) {
+      let roomID = createID();
+
+      let room = {
+        name: options.name,
+        isPrivate: options.isPrivate || false,
+        password: options.password || "",
+        maxUsers: options.maxUsers || 8,
+        users: []
+      };
+
+      ROOMS[roomID] = room;
+
+      socket.join(roomID);
+      socket.currentRoom = roomID;
+      ROOMS[roomID].users.push(socket.id);
+    } else {
+      let msg = "You have to set a name!";
+      socket.emit("create_room_error", msg);
+    }
+  });
+
+  // Joining Room
+  socket.on("join_room", data => {
+    let room = ROOMS[data.id];
+
+    if (typeof room == "undefined") {
+      let msg = "This room doesn't exist";
+      socket.emit("join_room_error", msg);
+      return false;
+    }
+
+    if (room.users.lenght < room.maxUsers) {
+      if (room.isPrivate) {
+        if (room.password == data.password) {
+          socket.join(data.id);
+          socket.currentRoom = data.id;
+          room.users.push(socket.id);
+        } else {
+          let msg = "Wrong password";
+          socket.emit("join_room_error", msg);
+        }
+      } else {
+        socket.join(data.id);
+        socket.currentRoom = data.id;
+        room.users.push(socket.id);
+      }
+    } else {
+      let msg = "There is max amount of users.";
+      socket.emit("join_room_error", msg);
+    }
+  });
+
+  // Leaving Room
+  socket.on("leave_room", id => {
+    if (ROOMS[id].includes(socket.id)) {
+      ROOMS[id].users.splice(ROOMS[id].indexOf(socket.id), 1);
+      socket.currentRoom = null;
+      socket.leave(id);
+      if (ROOMS[id].users.lenght == 0) {
+        delete ROOMS[id];
+      }
+    } else {
+      let msg = "You are not in this room.";
+      socket.emit("leave_room_error", msg);
+    }
   });
 });
 
