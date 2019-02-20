3b5a3efaade0695a3a7da404af8e7d38f4f2b065
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-02-20 20:36:25 +0100

    Updates

---
+++ precache-manifest.987bd5bf0741b4a754d481d8cc10ff47.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4133a81f09b7d4c6c75d",
+    "url": "/js/about.c7ac6903.js"
+  },
+  {
+    "revision": "bb8fc90374561ac13de8",
+    "url": "/css/app.982fbb57.css"
+  },
+  {
+    "revision": "bb8fc90374561ac13de8",
+    "url": "/js/app.32745958.js"
+  },
+  {
+    "revision": "1d1aff8995314ab4d4d9",
+    "url": "/js/chunk-2d21098f.9f2cd51e.js"
+  },
+  {
+    "revision": "4bf593455019704747f7",
+    "url": "/css/chunk-d499b55a.cf2c1808.css"
+  },
+  {
+    "revision": "4bf593455019704747f7",
+    "url": "/js/chunk-d499b55a.4a9a224c.js"
+  },
+  {
+    "revision": "6d3e5d7ef1d82d1c9b7a",
+    "url": "/css/chunk-vendors.7fd3d809.css"
+  },
+  {
+    "revision": "6d3e5d7ef1d82d1c9b7a",
+    "url": "/js/chunk-vendors.90932735.js"
+  },
+  {
+    "revision": "d23a11095b549c10efad67b5b6b71314",
+    "url": "/img/brush.d23a1109.svg"
+  },
+  {
+    "revision": "9ce1a9b3daa6e8a0ed5bc23a51bac6da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7e27fb43a3536c5fcdfe50c9778a17d",
+    "url": "/icons/brush.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
