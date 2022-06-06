779471053ef480c37d22bfd3c13fc1f73ea3acb8
Karol-Waliszewski <karolwaliszewski@gmail.com>
2022-06-06 08:58:34 +0200

    Changes to deploy heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import {
 } from "./services/socket";
 import { get } from "./utils/httpHelpers";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const httpServer = createServer(app);
 
